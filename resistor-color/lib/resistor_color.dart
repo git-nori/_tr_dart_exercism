72beac100c02bcbbfffe0d67cb03911fb30370cb
HironoriSato <maza.hs5360@gmail.com>
2021-03-13 21:57:42 +0900

    register-color submit

--- resistor-color/lib/resistor_color.dart
+++ resistor-color/lib/resistor_color.dart
@@ -1,1 +1,15 @@
-class ResistorColor {}
+class ResistorColor {
+  final colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
+  int colorCode(String code) => colors.indexOf(code);
+}
