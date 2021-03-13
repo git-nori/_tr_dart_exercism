b6006b1f06a658b52dfc28537e81e2cccdd4c829
HironoriSato <maza.hs5360@gmail.com>
2021-03-13 22:47:54 +0900

    resistor-color-duo submit

--- resistor-color-duo/lib/resistor_color_duo.dart
+++ resistor-color-duo/lib/resistor_color_duo.dart
@@ -1,3 +1,18 @@
 class ResistorColorDuo {
-  // Put your code here
+  final colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
+  int value(List<String> colorsList) => int.parse(colorsList
+      .sublist(0, 2)
+      .map((e) => colors.indexOf(e).toString())
+      .fold<String>('', (previousValue, element) => previousValue + element));
 }
