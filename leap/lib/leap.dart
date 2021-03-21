09c11e6d1b28c05a7b05af65b7a5e96ed289d509
HironoriSato <maza.hs5360@gmail.com>
2021-03-21 22:33:52 +0900

    submit

--- leap/lib/leap.dart
+++ leap/lib/leap.dart
@@ -1,3 +1,7 @@
 class Leap {
-  // Put your code here
+  bool leapYear(int year) {
+    if (year % 4 != 0) return false;
+    if (year % 100 == 0 || year % 400 == 0) return false;
+    return true;
+  }
 }
